39088c438b47cd2594abacce8f8895abf2384f05
Ecce Houenou <fivode.houenou@gmail.com>
2025-11-07 15:17:19 -0500

    answer3b

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -33,7 +33,7 @@ mkdir data
 mv ./rawdata ./data/raw
 # 3. List the contents of the ./data/raw directory
 cd ./data/raw
-ls 
+ls data/raw
 # 4. In ./data/processed, create the following directories: server_logs, user_logs, and event_logs
 #cd ..
 #cd ./data/processed
