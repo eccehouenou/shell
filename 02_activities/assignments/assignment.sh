8a944fd858f5b4241a5359046274ff1e11e8cfda
Ecce Houenou <fivode.houenou@gmail.com>
2025-11-07 17:42:24 -0500

    Answer 8 b

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -50,7 +50,7 @@ rm ./data/raw/*ipaddr* ./data/processed/user_logs/*ipaddr*
 #rm ./data/processed/user_logs/*ipaddr*
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
 
- ls -R /./data/processed > ./data/inventory.txt
+ ls -IR /./data/processed > ./data/inventory.txt
 ###########################################
 
 echo "Project setup is complete!"
